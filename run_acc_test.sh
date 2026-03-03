f293be1d87864c0f88496fcbb8a82af15bf105a1
letusss <weiliang_bai@foxmail.com>
2026-03-03 20:23:27 +0800

    modified accuracy test script

--- run_acc_test.sh
+++ run_acc_test.sh
@@ -13,4 +13,4 @@ DATALEN=16384
 
 # $PY test/eval_acc.py --datalen $DATALEN --method full --dataset_name $DATASETS --model_name "gradientai/Llama-3-8B-Instruct-Gradient-1048k" || exit 1
 
-$PY test/eval_acc.py --datalen $DATALEN --method experimental --dataset_name $DATASETS --model_name "gradientai/Llama-3-8B-Instruct-Gradient-1048k" --sparse_budget 2048 --rank 160 --chunk_size 8 || exit 1
+$PY test/eval_acc.py --datalen $DATALEN --method optimized --dataset_name $DATASETS --model_name "gradientai/Llama-3-8B-Instruct-Gradient-1048k" --sparse_budget 2048 --rank 160 --chunk_size 8 || exit 1
